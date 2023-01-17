f339ac5f31de6362507590f469aecb3a337aa993
Jun <rnsuz74@naver.com>
2023-01-18 00:28:10 +0900

    2023-01-18

--- Study/Keras/keras31_dropout2_california.py
+++ Study/Keras/keras31_dropout2_california.py
@@ -17,29 +17,12 @@ x_train, x_test, y_train, y_test=train_test_split(
     shuffle=True,
     random_state=333
 )
+
+#Scaler(데이터 전처리) 
 scaler = StandardScaler()
 # scaler = MinMaxScaler()
 x_train=scaler.fit_transform(x_train)
 x_test=scaler.transform(x_test)
-
-# 2.모델구성
-# model=Sequential()
-# model.add(Dense(50, input_dim=8, activation='relu'))
-# model.add(Dense(100, activation='relu'))
-# model.add(Dense(150, activation='relu'))
-# model.add(Dense(200, activation='relu'))
-# model.add(Dense(250, activation='relu'))
-# model.add(Dense(300, activation='relu'))
-# model.add(Dense(350, activation='relu'))
-# model.add(Dense(400, activation='relu'))
-# model.add(Dense(350, activation='relu'))
-# model.add(Dense(300, activation='relu'))
-# model.add(Dense(250, activation='relu'))
-# model.add(Dense(200, activation='relu'))
-# model.add(Dense(150, activation='relu'))
-# model.add(Dense(100, activation='relu'))
-# model.add(Dense(50, activation='relu'))
-# model.add(Dense(1))
 
 # 2.모델구성(함수형)
 input1=Input(shape=(8,))
@@ -49,15 +32,15 @@ dense3=Dense(150, activation='relu')(dense2)
 dense4=Dense(200, activation='relu')(dense3)
 dense5=Dense(250, activation='relu')(dense4)
 dense6=Dense(300, activation='relu')(dense5)
-drop1=Dropout(0.2)(dense6)
+drop1=Dropout(0.3)(dense6)
 dense7=Dense(350, activation='relu')(drop1)
-drop2=Dropout(0.3)(dense7)
+drop2=Dropout(0.4)(dense7)
 dense8=Dense(400, activation='relu')(drop2)
 drop3=Dropout(0.5)(dense8)
 dense9=Dense(350, activation='relu')(drop3)
-drop4=Dropout(0.3)(dense9)
+drop4=Dropout(0.4)(dense9)
 dense10=Dense(300, activation='relu')(drop4)
-drop5=Dropout(0.2)(dense10)
+drop5=Dropout(0.3)(dense10)
 dense11=Dense(250, activation='relu')(drop5)
 dense12=Dense(200, activation='relu')(dense11)
 dense13=Dense(150, activation='relu')(dense12)
@@ -73,17 +56,17 @@ model.compile(loss='mse', optimizer='adam')
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 earlyStopping = EarlyStopping(monitor='val_loss', 
                               mode='min', 
-                              patience=300, 
+                              patience=500, 
                               restore_best_weights=True, 
-                              verbose=1)
+                              verbose=2)
 
 import datetime
-date = datetime.datetime.now() #현재 시간 반환
+date = datetime.datetime.now() 
 print(date) 
-print(type(date)) #<class 'datetime.datetime'>
-date=date.strftime("%m%d_%H%M") #문자열 타입으로 변환
+print(type(date)) 
+date=date.strftime("%m%d_%H%M") 
 print(date) 
-print(type(date)) #<class 'str'>
+print(type(date)) 
 
 filepath='./_save/MCP/'
 filename='{epoch:04d}-{val_loss:.4f}.hdf5' 
@@ -91,26 +74,17 @@ filename='{epoch:04d}-{val_loss:.4f}.hdf5'
 #ModelCheckpoint
 ModelCheckpoint = ModelCheckpoint(monitor='val_loss',
                                   mode='auto',
-                                  verbose=1,
+                                  verbose=2,
                                   save_best_only=True,
                                   filepath=filepath+'k31_2_'+date+'_'+filename)
 
-hist = model.fit(x_train, y_train, epochs=10000, batch_size=150, 
-                 validation_split=0.25, callbacks=[earlyStopping, ModelCheckpoint], 
-                 verbose=1)  
+hist = model.fit(x_train, y_train, epochs=10000, batch_size=200, 
+                 validation_split=0.2, callbacks=[earlyStopping, ModelCheckpoint], 
+                 verbose=2)  
 
 #4.평가,예측
 loss=model.evaluate(x_test, y_test) 
 print('loss : ', loss)
-
-print("===============================")
-print(hist) #
-print("===============================")
-print(hist.history) 
-print("===============================")
-print(hist.history['loss'])   
-print("===============================")
-print(hist.history['val_loss'])   
 
 y_predict=model.predict(x_test)
 
@@ -121,11 +95,14 @@ print("RMSE : ", RMSE(y_test, y_predict))
 r2=r2_score(y_test,y_predict)
 print("R2 : ", r2)
 
-
-
-
-
-
+'''
+Epoch 00563: val_loss did not improve from 0.28264
+Epoch 00563: early stopping
+129/129 [==============================] - 0s 2ms/step - loss: 0.2827
+loss :  0.2827185094356537
+RMSE :  0.5317127633608937
+R2 :  0.7780995739992294
+'''
                   
 
 
