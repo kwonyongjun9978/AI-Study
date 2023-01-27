64787354dbe32a399d732be6ea1dcfff0334a6b4
Jun <rnsuz74@naver.com>
2023-01-27 12:10:25 +0900

    2023-01-27

--- Study/Keras/keras47_split2_1_LSTM.py
+++ Study/Keras/keras47_split2_1_LSTM.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import Dense, LSTM
 
 a = np.array(range(1, 101))
 
-timesteps = 5  #x는 4개, y는 1개
+timesteps = 5  
 
 def split_x(dataset, timesteps):
     aaa=[]
@@ -17,6 +17,7 @@ bbb = split_x(a, timesteps)
 print(bbb)
 print(bbb.shape) #(96, 5)
 
+#x는 4개, y는 1개
 x = bbb[:, :-1]
 y = bbb[:, -1]
 
@@ -24,11 +25,14 @@ print (x,y)
 print (x.shape, y.shape) # (96, 4) (96,)
 
 
-x_predict = np.array(range(96,106))  # 예상 y = 100, 107
+# 예상 y = 100~106
+x_predict = np.array(range(96,106))  
 
 x_predict = split_x(x_predict, 4) #timesteps = 4 
 print(x_predict)
+print(x_predict.shape) #(7, 4)
 
+#train_test_split
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(
     x,y,shuffle=True, random_state=444
@@ -60,21 +64,21 @@ model.summary()
 #3. 컴파일,훈련
 model.compile(loss='mse', optimizer='adam')
 
-model.fit(x,y,epochs=200, batch_size=8)
+model.fit(x_train,y_train,epochs=200, batch_size=8)
 
 #4.평가,예측
-loss=model.evaluate(x,y)
+loss=model.evaluate(x_test,y_test)
 print('loss : ', loss)
 result = model.predict(x_predict)
-print('결과 : ', result)
+print('100~106예측 결과 : ', result)
 
 '''
 loss :  0.0017833933234214783
-결과 :  [[ 99.99341 ]
-        [100.99639 ]
-        [101.99952 ]
-        [103.002716]
-        [104.00601 ]
-        [105.009445]
-        [106.01296 ]]
+100~106예측 결과 :  [[ 99.99341 ]
+                    [100.99639 ]
+                    [101.99952 ]
+                    [103.002716]
+                    [104.00601 ]
+                    [105.009445]
+                    [106.01296 ]]
 '''
